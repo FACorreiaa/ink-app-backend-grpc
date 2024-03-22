49682d8f14a54bf3a9d22b7aafa701fb5433b4a9
FACorreiaa <fernandocorreia316@gmail.com>
2024-03-22 23:11:49 +0000

    add new table

---
+++ internal/migrations/3_customers.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "costumer" (
+                        public_id VARCHAR(255) PRIMARY KEY,
+                        private_id VARCHAR(255),
+                        title VARCHAR(255),
+                        first VARCHAR(255),
+                        middle VARCHAR(255),
+                        last VARCHAR(255),
+                        suffix VARCHAR(255),
+                        nickname VARCHAR(255),
+                        email VARCHAR(255),
+                        phone VARCHAR(255),
+  -- Add other fields as needed
+);
