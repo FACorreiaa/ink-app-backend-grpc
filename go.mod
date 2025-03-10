b7f8b7bc536c8f1f33af87b91a7336bf25bafa4d
FACorreiaa <fernandocorreia316@gmail.com>
2025-03-10 16:45:50 +0100

    generate services for backoffice

--- go.mod
+++ go.mod
@@ -2,8 +2,6 @@ module github.com/FACorreiaa/ink-app-backend-grpc
 
 go 1.24.1
 
-toolchain go1.24.0
-
 require (
 	github.com/FACorreiaa/ink-app-backend-protos v0.0.0-20250310151626-4d1c0cf157b5
 	github.com/golang-jwt/jwt/v5 v5.2.1
@@ -31,7 +29,6 @@ require (
 )
 
 require (
-	github.com/FACorreiaa/fitme-protos v0.0.0-20250218122301-58600ee7869e // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
--- internal/middleware.go
+++ internal/middleware.go
@@ -2,13 +2,11 @@ package internal
 
 import (
 	"github.com/FACorreiaa/ink-app-backend-protos/container"
-	"github.com/FACorreiaa/ink-app-backend-protos/modules/customer"
-	"github.com/FACorreiaa/ink-app-backend-protos/modules/user"
-
-	"github.com/FACorreiaa/ink-app-backend-grpc/config"
 
 	"github.com/FACorreiaa/ink-app-backend-protos/utils"
 	"go.uber.org/zap"
+
+	"github.com/FACorreiaa/ink-app-backend-grpc/config"
 )
 
 // ConfigureUpstreamClients maintains the broker container so we have a struct that we can pass
@@ -21,7 +19,7 @@ func ConfigureUpstreamClients(log *zap.Logger, transport *utils.TransportUtils) 
 		return nil
 	}
 
-	cfg, err := config.InitConfig()
+	_, err := config.InitConfig()
 	if err != nil {
 		log.Error("failed to initialize config")
 		return nil
@@ -29,18 +27,18 @@ func ConfigureUpstreamClients(log *zap.Logger, transport *utils.TransportUtils) 
 	// If you have a lot of upstream services, you'll probably want to use an
 	// itt here instead, but for the example we've only got the one.
 
-	customerBroker, err := customer.NewBroker(cfg.UpstreamServices.Customer)
-	if err != nil {
-		log.Error("failed to create customer service broker", zap.Error(err))
-		return nil
-	}
-
-	authBroker, err := user.NewBroker(cfg.UpstreamServices.Customer)
-	if err != nil {
-		log.Error("failed to create auth service broker", zap.Error(err))
-		return nil
-	}
-	brokers.Customer = customerBroker
-	brokers.Auth = authBroker
+	//customerBroker, err := customer.NewBroker(cfg.UpstreamServices.Customer)
+	//if err != nil {
+	//	log.Error("failed to create customer service broker", zap.Error(err))
+	//	return nil
+	//}
+	//
+	//authBroker, err := user.NewBroker(cfg.UpstreamServices.Customer)
+	//if err != nil {
+	//	log.Error("failed to create auth service broker", zap.Error(err))
+	//	return nil
+	//}
+	//brokers.Customer = customerBroker
+	//brokers.Auth = authBroker
 	return brokers
 }
